fe9c3769dd6d673f8bd4c19b50ed7fccf0d937cb
programadriano <thiago.adriano26@gmail.com>
2019-09-08 22:44:48 -0300

    first commit

---
+++ src/controllers/personController.js
@@ -0,0 +1,29 @@
+var request = require("request");
+
+exports.get = async (req, res, next) => {
+    await request("http://www.google.com", function(error, response, body) {
+      res.json(body);
+    });
+  };
+
+
+exports.getById = (req, res, next) => {
+    res.status(200).send('Requisição recebida com sucesso!');
+};
+
+
+
+exports.post = (req, res, next) => {
+    res.status(201).send('Requisição recebida com sucesso!');
+};
+
+
+exports.put = (req, res, next) => {
+    let id = req.params.id;
+    res.status(201).send(`Requisição recebida com sucesso! ${id}`);
+};
+
+exports.delete = (req, res, next) => {
+    let id = req.params.id;
+    res.status(200).send(`Requisição recebida com sucesso! ${id}`);
+};
